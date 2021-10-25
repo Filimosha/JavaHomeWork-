4062f481dffed1e306fc3db100bb74c102ed2ff8
Inna Sergeychik <inna.sergejchik@privatbank.ua>
2021-10-25 17:56:13 +0300

    init commit

---
+++ src/com/pb/sergeychik/hw3/Array.java
@@ -0,0 +1,45 @@
+package com.pb.sergeychik.hw3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Array<isSorted> {
+    public static void main(String[] args) {
+        System.out.println("Введите масив");
+        int[] array = new int[10];
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
+        }
+        int sum = 0;
+        for (int j : array) {
+            sum = sum + j;
+        }
+        System.out.println("Сума массива:" + (sum));
+
+        int count = 0;
+        for (int j : array) {
+            if (j > 0) {
+                count++;
+            }
+        }
+        System.out.println("В массиве " + Arrays.toString(array));
+        System.out.println(count + " положительных элементов");
+
+        boolean isSorted = false;
+        int buf;
+        while(!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    isSorted = false;
+
+                    buf = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = buf;
+                }
+            }
+            System.out.println("Сортировка пузырьком"+ Arrays.toString( array));
+        }
+    }
+}
